1ce989f3bad945fb16a65567fdb9430b7bee7c9c
nivrimn <nivrimn@gmail.com>
2024-03-26 19:20:54 +0200

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Container, Row, Col } from "react-bootstrap"
 import headerImg from '../assets/img/header-img.svg'
 import { ConnectButton } from '../components/ConnectButton'
@@ -7,37 +7,36 @@ import './Banner.css';
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
-    const[isDeleting, setIsDeleting] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100)
-    const toRotate = ["Software Developer","Backend Developer"];
+    const toRotate = ["Software Developer", "Backend Developer"];
     const period = 2000;
 
-    useEffect (() => {
+    useEffect(() => {
         let ticker = setInterval(() => {
             tick();
         }, delta);
-    
+        
         return () => {
             clearInterval(ticker);
         };
-    }, [text, delta]);
-
-    const tick = () =>
-    {
+    }, [text, delta, tick]);
+    
+    const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
 
         setText(updatedText);
 
-        if(isDeleting) {
+        if (isDeleting) {
             setDelta(prevDelta => prevDelta / 2)
         }
-        if(!isDeleting && updatedText === fullText) {
+        if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
             setDelta(period);
-        } else if(isDeleting && updatedText === '') {
+        } else if (isDeleting && updatedText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
             setDelta(500);
@@ -50,12 +49,12 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7} >
-                    <h1>{`Hi I'm Niv Rimon🎗️ -`}<span className="wrap"> {text} </span></h1>
-                    <p>I'm a software engineer. I have profound interest in software development, devops, AI, and everything in between.</p>
-                    <ConnectButton/>
+                        <h1>{`Hi I'm Niv Rimon🎗️ -`}<span className="wrap"> {text} </span></h1>
+                        <p>I'm a software engineer. I have profound interest in software development, devops, AI, and everything in between.</p>
+                        <ConnectButton />
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img"/>
+                        <img src={headerImg} alt="Header Img" />
                     </Col>
                 </Row>
             </Container>
