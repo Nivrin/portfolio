6b61593fa2b05987f1bc38c762bdc98f48c54784
nivrimn <nivrimn@gmail.com>
2024-03-26 20:10:39 +0200

    improved about text

--- src/components/About.js
+++ src/components/About.js
@@ -15,11 +15,17 @@ export const About = () => {
           <h2>About</h2>
           </Col>
             <Col xs={12} md={6} xl={7}>
-              <p>I'm experienced Software Developer with a background in an IDF technology unit and a small startup,
-                currently working full-time at reserve military.
-                I'm highly motivated team player, quick learner, and socially adept.
-                Excels under pressure, guiding and presenting confidently to large groups.
-                currently, I'm actively seeking a new role as Backend Developer or DevOps. </p>
+              <p>I am an experienced Software Developer with a background in both 
+                an IDF technology unit and a small startup. Throughout my career, 
+                I have worked with a diverse range of technologies, specializing
+                 in backend development. Currently, I am serving full-time in the reserve military.
+                 </p>
+                 <p>
+                  
+                  My experience has honed my skills in navigating complex challenges, leading teams under pressure, 
+                  and presenting confidently to large groups. I am known for my ability to adapt quickly to new technologies 
+                  and environments, as well as my strong interpersonal skills. I am actively seeking opportunities to apply my 
+                  expertise as a Backend Developer or DevOps </p>
             </Col>
             <Col xs={12} md={6} xl={5}>
               <img src={nivPorfile} alt="Header Img" />
