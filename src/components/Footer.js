da6b0dcf113f89d2fef0cffbbdfa00ceb0b40e35
nivrimn <nivrimn@gmail.com>
2024-03-28 14:42:45 +0200

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export const Footer = () => {
             <div className="social-icon">
              <SocialButtons/>
             </div>
-            <p>Copyright 2022. All Rights Reserved.</p>
+            {/* <p>Copyright 2022. All Rights Reserved.</p> */}
           </Col>
         </Row>
       </Container>
